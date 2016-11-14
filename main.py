440883314f0037c4734a95d07ba68194f6205acb
Chris Hill-Scott <me@quis.cc>
2016-11-14 12:50:42 +0000

    Add image generation

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ import os
 import subprocess
 from datetime import datetime
 
+now = datetime.utcnow()
+
 html_file = os.path.abspath('./letter.html')
-pdf_path = os.path.abspath('./output/example-{}.pdf'.format(datetime.utcnow()))
+pdf_path = os.path.abspath('./output/example-{}.pdf'.format(now))
 
 exit_code = subprocess.call([
     'wkhtmltopdf',
@@ -18,6 +20,20 @@ exit_code = subprocess.call([
     '10mm',
     'file://{}'.format(html_file),
     pdf_path,
+])
+
+exit_code = subprocess.call([
+    'pdftoppm',
+    '-singlefile',
+    '-png',
+    pdf_path,
+    'example-{}'.format(now),
+])
+
+exit_code = subprocess.call([
+    'mv',
+    'example-{}.png'.format(now),
+    'output/',
 ])
 
 if exit_code > 0:
